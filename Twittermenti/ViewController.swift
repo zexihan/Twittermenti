be9c4ed814385a7877c32113288260a31fbdb791
Zexi Han <zexihan@me.com>
2019-06-11 11:23:42 -0700

    Predict sentiment score of 100 tweets about a company

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SwifteriOS
 import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -23,12 +24,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company!")
+//        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company!")
+//
+//        print(prediction.label)
         
-        print(prediction.label)
-        
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: {(results, metadata) in
-            // print(results)
+        swifter.searchTweet(using: "@trump", lang: "en", count: 100, tweetMode: .extended, success: {(results, metadata) in
+            
+            var tweets = [TweetSentimentClassifierInput]()
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                    tweets.append(tweetForClassification)
+                }
+            }
+            
+            do {
+                let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                
+                var sentimentScore = 0
+                
+                for pred in predictions {
+                    let sentiment = pred.label
+                    
+                    if sentiment == "Pos" {
+                        sentimentScore += 1
+                    } else if sentiment == "Neg" {
+                        sentimentScore -= 1
+                    }
+                }
+                print(sentimentScore)
+            } catch {
+                print("There was an error with making a prediction, \(error)")
+            }
+            
+            
+            
         }) { (error) in
             print("There was an error with the Twitter API request, \(error)")
         }
