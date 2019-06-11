5eba82a1e6f61b63df5d4f4a5dd3689b563889db
Zexi Han <zexihan@me.com>
2019-06-11 11:04:23 -0700

    Create sentiment analysis model and request tweets using SwifteriOS

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = TweetSentimentClassifier()
+    
+    let swifter = Swifter(consumerKey: "LTReJyreyyuz4SWjVABuA1WKI", consumerSecret: "ajf107xfd6869heCG8Ptn8iztfFHBWPIxLkFGvsmoEwI7MKZcz")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company!")
+        
+        print(prediction.label)
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: {(results, metadata) in
+            // print(results)
+        }) { (error) in
+            print("There was an error with the Twitter API request, \(error)")
+        }
         
     }
 
